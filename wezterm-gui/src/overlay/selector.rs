c219bf7f7478d7963a428b7400b00cdbf1a483aa
JafarAbdi <jafar.uruc@gmail.com>
2026-08-07 01:34:53 +0100

    fix(selector): address review feedback
    Ctrl-u kills to start of line; only unmodified/shifted chars insert.

--- wezterm-gui/src/overlay/selector.rs
+++ wezterm-gui/src/overlay/selector.rs
@@ -55,6 +55,13 @@ struct SelectorState {
 }
 
 impl SelectorState {
+    fn kill_filter_text(&mut self, movement: Movement) {
+        let mut buffer = LineEditBuffer::new(&self.filter_term, self.filter_term.len());
+        buffer.kill_text(movement, movement);
+        self.filter_term = buffer.get_line().to_string();
+        self.update_filter();
+    }
+
     fn update_filter(&mut self) {
         if self.filter_term.is_empty() {
             self.filtered_entries = self.args.choices.clone();
@@ -316,21 +323,21 @@ impl SelectorState {
                     key: KeyCode::Char('W'),
                     modifiers: Modifiers::CTRL,
                 }) if self.filtering => {
-                    let mut buffer = LineEditBuffer::new(&self.filter_term, self.filter_term.len());
-                    buffer.kill_text(Movement::BackwardWord(1), Movement::BackwardWord(1));
-                    self.filter_term = buffer.get_line().to_string();
-                    self.update_filter();
+                    self.kill_filter_text(Movement::BackwardWord(1));
                 }
                 InputEvent::Key(KeyEvent {
                     key: KeyCode::Char('U'),
                     modifiers: Modifiers::CTRL,
                 }) if self.filtering => {
-                    self.filter_term.clear();
-                    self.update_filter();
+                    self.kill_filter_text(Movement::StartOfLine);
                 }
                 InputEvent::Key(KeyEvent {
                     key: KeyCode::Char(c),
-                    ..
+                    modifiers: Modifiers::NONE,
+                })
+                | InputEvent::Key(KeyEvent {
+                    key: KeyCode::Char(c),
+                    modifiers: Modifiers::SHIFT,
                 }) if self.filtering => {
                     self.filter_term.push(c);
                     self.update_filter();
